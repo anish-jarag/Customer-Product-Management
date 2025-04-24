a63415f24d6b4008ef884206698fe743bad05bfb
effat07 <effatmujawar26@gmail.com>
2025-04-24 21:35:05 +0530

    Updated backend

--- backend/routes/customerRoutes.js
+++ backend/routes/customerRoutes.js
@@ -4,9 +4,19 @@ const router = express.Router();
 const {
   addCustomer,
   getCustomers,
+  getCustomerById,
+  updateCustomer,
+  deleteCustomer,
 } = require("../controllers/customerController");
 
 router.post("/", addCustomer);
+
 router.get("/", getCustomers);
+
+router.get("/:id", getCustomerById);
+
+router.put("/:id", updateCustomer);
+
+router.delete("/:id", deleteCustomer);
 
 module.exports = router;
